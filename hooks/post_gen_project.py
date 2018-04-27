359f9dc480f417b9acc2bb08a7c4f2d953863ded
Mario Costa <mario@eyeware.tech>
2018-04-27 14:59:11 +0100

    Removed external directory, will be using conan for dependencies.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,7 @@
 
 if __name__ == '__main__':
     """Initialize a git repository based on the configured branch and repo"""
-    url    = 'git@{{ cookiecutter.git_server }}:{{ cookiecutter.username }}/{{ cookiecutter.project_slug }}'
+    url    = 'git@{{ cookiecutter.git_server }}:{{ cookiecutter.username }}/{{ cookiecutter.project_slug }}.git'
     branch = '{{ cookiecutter.branch }}'
     remote = '{{ cookiecutter.remote }}'
 
