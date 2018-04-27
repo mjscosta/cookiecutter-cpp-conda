a70af5af308bbef9a3a96125bafa1304bfbd6b0d
Mario Costa <mario@eyeware.tech>
2018-04-27 11:56:36 +0100

    Fixed some bad variables.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,11 +2,7 @@
 
 if __name__ == '__main__':
     """Initialize a git repository based on the configured branch and repo"""
-<<<<<<< HEAD
-    url    = 'git@{{ cookiecutter.git_server }}:{{ cookiecutter.username }}/{{ cookiecutter.repo_name }}'
-=======
-    url    = 'git@github.com:{{ cookiecutter.username }}/{{ cookiecutter.repo_name }}'
->>>>>>> 6a215f53f9bdc302bea269e7f1eeffc9cc02ce91
+    url    = 'git@{{ cookiecutter.git_server }}:{{ cookiecutter.username }}/{{ cookiecutter.project_slug }}'
     branch = '{{ cookiecutter.branch }}'
     remote = '{{ cookiecutter.remote }}'
 
