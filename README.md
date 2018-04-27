08f8dde4347d4291327eac8e6b5ae42fc513a346
Mario Costa <mario@eyeware.tech>
2018-04-27 11:13:31 +0100

    Modidifies repo_name by project slug, to be used in other places, project name might be a bad name with spaces and so on.

---
+++ README.md
@@ -0,0 +1,12 @@
+cookiecutter-cpp
+================
+
+This work is a modified version of https://github.com/hbristow/cookiecutter-cpp.
+
+A test-driven C++ project template built on CMake, Catch2 and conan.
+
+Usage:
+------
+
+    $ pip install cookiecutter>=1.4.0
+    $ cookiecutter gh:eyeware/cookiecutter-cpp
---
+++ hooks/post_gen_project.py
@@ -0,0 +1,13 @@
+"""Post-project generation hooks"""
+
+if __name__ == '__main__':
+    """Initialize a git repository based on the configured branch and repo"""
+    url    = 'git@{{ cookiecutter.git_server }}:{{ cookiecutter.username }}/{{ cookiecutter.repo_name }}'
+    branch = '{{ cookiecutter.branch }}'
+    remote = '{{ cookiecutter.remote }}'
+
+    import subprocess
+    subprocess.call(('git', 'init'))
+    subprocess.call(('git', 'checkout', '-b', branch))
+    subprocess.call(('git', 'remote', 'add', remote, url))
+    subprocess.call(('mv', 'pre-commit', '.git/hooks/'))
