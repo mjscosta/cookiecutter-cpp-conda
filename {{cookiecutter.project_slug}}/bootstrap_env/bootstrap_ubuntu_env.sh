04bd25a8b9f81911dd74b796cd883572e3d790b2
Mario Costa <you@example.com>
2018-11-06 12:38:23 +0000

    Initial version, with conda and bumpversion, and ubuntu bootstrap

---
+++ {{cookiecutter.project_slug}}/bootstrap_env/bootstrap_ubuntu_env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# @author: Mario Costa
+# @date: 06/11/2018
+ 
+# Ubunto development environment bootstrap script
+
+#------------------------------------------------------------------------------
+#            NOTE: DO NOT ADD LIBRARY DEPENDENCIES TO THIS FILE
+#------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+# Add here ubuntu specific tools that are not available via anaconda cloud 
+# packages.
+#------------------------------------------------------------------------------
+
+sudo apt-get install -y clang-format-4.0
